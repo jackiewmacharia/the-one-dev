231d84360d0cf48032f3cc6a9289db908742e4b7
Jackie Macharia <jacklinewmacharia@gmail.com>
2023-01-06 02:14:42 +0300

    fix: fix linting

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'cypress';
+import { defineConfig } from "cypress";
 
 export default defineConfig({
   projectId: "spdb6n",
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    baseUrl: "http://localhost:3000",
     supportFile: false,
   },
 });
