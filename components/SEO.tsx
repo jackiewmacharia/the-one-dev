8b7daaeff32af63e30e19807388e920faa77476e
Jackie Macharia <jacklinewmacharia@gmail.com>
2023-01-06 14:46:42 +0300

    fix: fix meta tags

--- components/SEO.tsx
+++ components/SEO.tsx
@@ -3,27 +3,49 @@ import Head from "next/head";
 export default function SEO({
   title,
   description,
+  author,
+  date,
 }: {
   title: string;
   description: string;
+  author?: string;
+  date?: string;
 }) {
   return (
     <Head>
       <title>{title}</title>
 
-      <meta name="description" content={description} />
-
-      {/* Twitter */}
-      <meta name="twitter:card" content="summary" />
-      <meta name="twitter:creator" content="@jackiewmacharia" />
-
       {/* Open Graph */}
+      <meta
+        name="description"
+        property="og:description"
+        content={description}
+      />
+      <meta name="author" content={author || ""} />
+      <meta
+        name="publish_date"
+        property="og:publish_date"
+        content={
+          (date &&
+            new Intl.DateTimeFormat("en-US", {
+              dateStyle: "full",
+              timeStyle: "long",
+            }).format(new Date(date))) ||
+          ""
+        }
+      />
       <meta property="og:title" content={title} />
       <meta property="og:url" content="https://theonedev.blog" />
       <meta property="og:site_name" content={title} />
       <meta property="og:type" content="website" />
-      <meta property="og:description" content={description} />
-      <meta property="og:image" content="/banners/the-one-dev-orng.jpg" />
+      <meta property="og:image" content="/banners/the-one-dev-tw-card.jpg" />
+
+      {/* Twitter */}
+      <meta name="twitter:card" content={description} />
+      <meta name="twitter:site" content="@jackiewmacharia" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content="/banners/the-one-dev-tw-card.jpg" />
     </Head>
   );
 }
