a47a4a3a1ab20090ece2b0b9a14607bed627f4d9
Jackie Macharia <jacklinewmacharia@gmail.com>
2023-01-16 18:30:55 +0300

    fix: troubleshoot meta tags

--- components/SEO.tsx
+++ components/SEO.tsx
@@ -1,28 +1,33 @@
 import Head from "next/head";
+import { useEffect, useState } from "react";
 
 export default function SEO({
   title,
   description,
+  domain,
   author,
   date,
   author_handle,
 }: {
   title: string;
   description: string;
+  domain: string;
   author?: string;
   date?: string;
   author_handle?: string;
 }) {
+  const [url, setUrl] = useState(`https://${domain}`);
+
+  useEffect(() => {
+    setUrl(window.location.href);
+  });
+
   return (
     <Head>
       <title>{title}</title>
 
       {/* Open Graph */}
-      <meta
-        name="description"
-        property="og:description"
-        content={description}
-      />
+      <meta name="description" content={description} />
       <meta name="author" content={author || ""} />
       <meta
         name="publish_date"
@@ -37,21 +42,30 @@ export default function SEO({
         }
       />
       <meta property="og:title" content={title} />
-      <meta property="og:url" content="https://theonedev.blog" />
+      <meta property="og:url" content={url} />
       <meta property="og:site_name" content={title} />
       <meta property="og:type" content="website" />
-      <meta property="og:image" content="https://theonedev.blog/banners/the-one-dev-tw-card.jpg" />
+      <meta property="og:description" content={description} />
+      <meta
+        property="og:image"
+        content="https://theonedev.blog/banners/the-one-dev-tw-card.jpg"
+      />
 
       {/* Twitter */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content="@jackiewmacharia" />
+      <meta property="twitter:domain" content={domain} />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta property="twitter:url" content={url} />
       <meta
         name="twitter:creator"
         content={`@${author_handle}` || "@jackiewmacharia"}
       />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content="https://theonedev.blog/banners/the-one-dev-tw-card.jpg" />
+      <meta
+        name="twitter:image"
+        content="https://theonedev.blog/banners/the-one-dev-tw-card.jpg"
+      />
 
       {/* Google site verification */}
       <meta
